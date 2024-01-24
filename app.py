8b1c892e98c16d0b5dcc7df12c06d856dcf6de13
TunaPig1228 <132420858+TunaPig1228@users.noreply.github.com>
2024-01-24 12:55:42 +0800

    Create app.py
    Build chatgpt python flask.

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import g4f
+
+app = Flask(__name__)
+
+@app.route('/transfer_data', methods=['POST'])
+def transfer_data():
+    if request.method == "POST":
+      query = request.json['query']  # 取得POST傳遞的JSON資訊
+      g4f.debug.logging = False
+      g4f.debug.version_check = False
+      response = g4f.ChatCompletion.create(
+        model = g4f.models.gpt_4,
+        messages = [{"role": "user", "content": query}],
+      )
+      return response
+    else:
+      return "Unsupported Method"
+
+if __name__ == '__main__':
+    app.run()
